35bc462d2b31e6070bffa3e18d42b2b740604cca
Mikias Abera <itsmikias@gmail.com>
2018-12-05 11:54:39 -0500

    Add posts route, use 404 redirect for SPA

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root 'pages#index'
-  match '*path' => redirect('/'), via: :get
+  get '404', to: 'pages#index', via: :get
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: :index
+    end
+  end
 end
