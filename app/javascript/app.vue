9c04b472957474fba5374ca5a3b52a8d4e8318d7
Mikias Abera <itsmikias@gmail.com>
2018-12-05 11:57:36 -0500

    Create App component, requests posts and display in list.

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,48 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div>
+    <h1>Posts</h1>
+    <div v-if="error" class="error">{{error}}</div>
+    <div class="loading" v-if="loading">Loading...</div>
+    <ul>
+      <li v-for="post of posts" :key="post.id">
+        <h3>{{post.title}}</h3>
+        <p v-html="post.preview()"></p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import {Post} from './models/post.model'
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      posts: [],
+      error: null,
+      loading: true
     }
+  },
+
+  created() {
+    Post.all()
+        .then(res => {
+          this.posts = res.data
+          this.loading = false
+        })
+        .catch(err => {
+          this.error = err
+          this.loading = false           
+        })
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
+h1 {
   text-align: center;
+}
+ul {
+  list-style: none;
 }
 </style>
