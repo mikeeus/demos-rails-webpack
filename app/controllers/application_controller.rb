72690130130b2b3ed2e435b871daf657307dd9cd
Mikias Abera <itsmikias@gmail.com>
2018-12-04 23:00:21 -0500

    Setup application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  include Graphiti::Rails
+
+  # When #show action does not find record, return 404
+  register_exception Graphiti::Errors::RecordNotFound, status: 404
+
+  rescue_from Exception do |e|
+    handle_exception(e)
+  end
 end
