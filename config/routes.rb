3ba4f3832f115841577cf98c39ddcd40ad90239d
Mikias Abera <itsmikias@gmail.com>
2018-12-05 12:12:33 -0500

    Remove via call

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'pages#index'
-  get '404', to: 'pages#index', via: :get
+  get '404', to: 'pages#index'
 
   namespace :api do
     namespace :v1 do
